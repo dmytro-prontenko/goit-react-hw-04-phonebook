d6704f22bb055b0d08364c7b5a38da320159737a
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2023-09-19 15:18:08 +0200

    app hooks done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,91 +1,108 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ContactsList from './ContactsList/ContactsList';
 import Filter from './Filter/Filter';
 import Form from './Form/Form';
 import { nanoid } from 'nanoid';
 import { toast } from 'react-toastify';
 
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-const INITIAL_STATE = {
-  contacts: [],
-  filter: '',
-};
-
-class App extends React.Component {
-  state = {
-    ...INITIAL_STATE,
+  const mapState = {
+    filter: setFilter,
   };
 
-  componentDidMount(){
-    const contacts = JSON.parse(window.localStorage.getItem("contacts"))
-    if (contacts?.length) {
-      this.setState({contacts})
-    }
-  }
+  useEffect(()=>{
+    const contactsFromLocal = JSON.parse(window.localStorage.getItem("contacts"))
+        if (contactsFromLocal?.length) {
+          setContacts(contactsFromLocal)
+        }
+  },[])
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts.length !== this.state.contacts.length) {
-      window.localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
-    }
-  }
+  useEffect((prev)=>{
+    if (prev !== contacts) {
+            window.localStorage.setItem("contacts", JSON.stringify(contacts))
+          }
+  },[contacts])
 
-  handleChangeInput = e => {
-    console.log(e.target.name);
-    this.setState({ [e.target.name]: e.target.value });
+  const handleChangeInput = e => {
+    const { name, value } = e.target;
+    mapState[name](value);
   };
 
-  handleAddContact = ({ name, number }) => {
-    const contactExists = this.state.contacts.some(
-      contact => contact.name === name
-    );
+  const handleAddContact = ({ name, number }) => {
+    const contactExists = contacts.some(contact => contact.name === name);
     if (name && number) {
       if (!contactExists) {
-
-        this.setState(prev => ({
-          contacts: [...prev.contacts, { id: nanoid(), name, number }],
-          name: '',
-          number: '',
-        }));
+        setContacts(prev => [...prev, { id: nanoid(), name, number }]);
         toast.success(`${name} was added to contacts`);
       } else {
         toast.error(`${name} is already exist in contacts`);
-        this.setState({ name: '', number: '' });
       }
     }
   };
 
-  handleDeleteContact = id => {
-    this.setState(prev => ({contacts: [...prev.contacts.filter(contact => contact.id !== id )]}))
-  }
+  const handleDeleteContact = id => {
+    setContacts(prev => prev.filter(contact => contact.id !== id));
+  };
 
-  filteredContacts = () => {
-    return this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+  const filteredContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-    const filteredData = this.filteredContacts();
-    return (
-      <div className="wrapper">
-        <h1>Phonebook</h1>
-        <Form addContact={this.handleAddContact} />
-        {contacts.length ? (
-          <>
-            <h2>Contacts</h2>
-            <Filter
-              onFilterChange={this.handleChangeInput}
-              filterValue={filter}
-            />
-            <ContactsList contacts={filteredData} filterValue={filter} deleteContact={this.handleDeleteContact}/>
-          </>
-        ) : (
-          'There are no contacts'
-        )}
-      </div>
-    );
-  }
-}
+  const filteredData = filteredContacts();
+
+  return (
+    <div className="wrapper">
+      <h1>Phonebook</h1>
+      <Form addContact={handleAddContact} />
+      {contacts.length ? (
+        <>
+          <h2>Contacts</h2>
+          <Filter onFilterChange={handleChangeInput} filterValue={filter} />
+          <ContactsList
+            contacts={filteredData}
+            filterValue={filter}
+            deleteContact={handleDeleteContact}
+          />
+        </>
+      ) : (
+        'There are no contacts'
+      )}
+    </div>
+  );
+};
 
 export default App;
+
+// const INITIAL_STATE = {
+//   contacts: [],
+//   filter: '',
+// };
+
+// class App extends React.Component {
+//   state = {
+//     ...INITIAL_STATE,
+//   };
+
+//   componentDidMount(){
+//     const contacts = JSON.parse(window.localStorage.getItem("contacts"))
+//     if (contacts?.length) {
+//       this.setState({contacts})
+//     }
+//   }
+
+//   componentDidUpdate(_, prevState) {
+//     if (prevState.contacts.length !== this.state.contacts.length) {
+//       window.localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
+//     }
+//   }
+
+// handleChangeInput = e => {
+//   console.log(e.target.name);
+//   this.setState({ [e.target.name]: e.target.value });
+// };
+
